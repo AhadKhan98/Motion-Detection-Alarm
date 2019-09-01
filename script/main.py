ebce3f0e5b1c08caa3abab8b927e58f3d3e7399c
AhadKhan98 <ahadkhan98@hotmail.com>
2019-09-01 11:05:55 -0400

    Added time recording functionality

--- script/main.py
+++ script/main.py
@@ -1,6 +1,24 @@
+from webcam import Camera
+from tkinter import *
 
 def main():
-    print(1)
+    print("Welcome to the Motion Detection Alarm. \nHow sensitive do you want the motion detection? (1=Low, 2=Med, 3=High)")
+
+    while True:
+        try:
+            sensitivity = int(input("Enter Sensitivity: "))
+            break
+        except:
+            print("Please enter a valid number. (1=Low, 2=Med, 3=High)")
+
+    video = Camera()
+    if sensitivity == 1: video.start_capture(20000)
+    if sensitivity == 2: video.start_capture(15000)
+    if sensitivity == 3: video.start_capture(10000)
+    window = Tk()
+    btn = Button(window,text="Intruder!")
+    btn.pack(fill=BOTH,expand=1)
+    window.mainloop()
 
 if __name__=="__main__":
     main()
--- script/webcam.py
+++ script/webcam.py
@@ -1,11 +1,15 @@
-import cv2,time
+import cv2,time,pandas
+from datetime import datetime
 
 class Camera:
-    def start_capture(self):
+    def start_capture(self,sensitivity):
         first_frame = None
+        motion_list = [None,None] # Stores values for motion and no motion
+        motion_times = [] # Stores times where each object entered and exited the frame
+        df = pandas.DataFrame(columns=["Start","End"])
         video = cv2.VideoCapture(0)
-
         while True:
+            motion = 0
             check, frame = video.read()
             gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) # Creates a grayscale version of the video frame
             gray_frame = cv2.GaussianBlur(gray_frame,(21,21),0) # Blurs the frame for better accuracy in motion detection
@@ -19,21 +23,33 @@ class Camera:
             thresh_frame = cv2.threshold(delta_frame,30,255,cv2.THRESH_BINARY)[1] # Stores the threshold frame from delta_frame
 
             (cnts,_) = cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) # Stores the contours from thresh_frame
+
             for c in cnts:
-                if cv2.contourArea(c) < 10000: # Checks if the motion was greater than a certain amount
+                if cv2.contourArea(c) < sensitivity: # Checks if the motion was greater than a certain amount
                     continue
                 else:
+                    motion = 1
                     (x,y,w,h) = cv2.boundingRect(c) # stores dimensions for rectangle around moving object
                     cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),3) # draws the rectangle on the displayed video
-
+            motion_list += [motion]
+            if motion_list[-1] == 1 and motion_list[-2] == 0: # Motion to No Motion
+                motion_times += [datetime.now().time().strftime('%H:%M:%S')]
+            if motion_list[-1] == 0 and motion_list[-2] == 1: # No Motion to Motion
+                motion_times += [datetime.now().time().strftime('%H:%M:%S')]
             cv2.imshow('Motion Detection Alarm',frame)
 
             key_press = cv2.waitKey(1)
             if key_press ==ord('q'):
+                if motion == 1: # Adds the time for last object if application was quit
+                    motion_times += [datetime.now()]
                 break
+
+        for i in range(0,len(motion_times),2):
+            df=df.append({"Start":motion_times[i],"End":motion_times[i+1]},ignore_index=True)
+        df.to_csv("Times.csv")
+
         video.release()
         cv2.destroyAllWindows()
 
-
 cam = Camera()
-cam.start_capture()
+cam.start_capture(10000)
